cf20a384a198207127bf95f292762f6d614f6e2b
weizq <ziqi0129!!!>
2019-12-13 17:00:17 +0800

    2019/12/13 --lastKhadgar

--- publicTools/animation.js
+++ publicTools/animation.js
@@ -1,10 +1,15 @@
-function moveDom (dom, target) {
-    let attrTarget = target[2]
-    let interval = setInterval(() => {
-        if (Math.abs(dom[target[0]] - attrTarget) > 5) {
-                dom.style[target[1]] = dom[target[0]] - (dom[target[0]] - attrTarget) / 50 + 'px'
+function moveDom (dom, target, callback) {
+    var attrTarget = target[2]
+    var distance = Math.abs(dom[target[0]] - attrTarget)
+    var interval = setInterval(() => {
+        if (Math.abs(dom[target[0]] - attrTarget) > distance / 100) {
+            dom.style[target[1]] = dom[target[0]] -
+                (dom[target[0]] - attrTarget > 0 ? Math.ceil((dom[target[0]] - attrTarget) / 50) : Math.floor((dom[target[0]] - attrTarget) / 50)) + 'px'
         } else {
-            dom.style[target[1]] = attrTarget
+            dom.style[target[1]] = attrTarget + 'px'
+            if (callback) {
+                callback()
+            }
             clearInterval(interval)
         }
     }, 10)
@@ -31,4 +36,40 @@ function scrollListener (dom, top, callback) {
             callback(3, scrollTop)
         }
     });
+}
+
+var interval = []
+function scaleDom (domList, target) {
+    for (var i = 0; i < domList.length; i++) {
+        (function () {
+            var targett = 1 - Math.abs(target - i)/10
+            var st = window.getComputedStyle(domList[i], null)
+            var tr = st.getPropertyValue("-webkit-transform") ||
+                st.getPropertyValue("transform")
+            var start = parseFloat(tr.split('(')[1].split(',')[0])
+            if (!isNaN(start)) {
+                var a = scaleDom1(domList, i, start, targett)
+                a()
+            }
+        })()
+    }
+}
+
+function scaleDom1 (domList, i, start, targett) {
+    return function () {
+        if (interval[i]) {
+            clearInterval(interval[i])
+        }
+        interval[i] = setInterval(function () {
+            if (Math.abs(targett - start) > 0.005) {
+                start = start + (targett - start) / 20
+                domList[i].style.transform = 'scale(' + start + ')'
+                domList[i].style.webkitTransform = 'scale(' + start + ')'
+            } else {
+                domList[i].style.transform = 'scale(' + targett + ')'
+                domList[i].style.webkitTransform = 'scale(' + targett + ')'
+                clearInterval(interval[i])
+            }
+        }, 10)
+    }
 }
